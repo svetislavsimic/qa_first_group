b983c88aa8f26747eb0bd1235ef67f40244cd7a2
sima <svetislavsimic77@gmail.com>
2019-02-10 01:06:47 +0100

    Add font increase and decrease, also add menu bar

--- my_notepad/mainWindow.py
+++ my_notepad/mainWindow.py
@@ -2,23 +2,47 @@ import tkinter as tk
 import tkinter.scrolledtext as tkst
 import tkinter.font as tkFont
 
+class App:
+    def __init__(self):
+        win = tk.Tk()
+        self.customFont = tkFont.Font(family="Helvetica", size=14)
+
+        frame1 = tk.Frame(
+            master=win,
+            # background = 'maroon'
+        )
+        frame1.pack(fill='both', expand='yes')
+        editArea = tkst.ScrolledText(
+            master=frame1,
+            wrap=tk.WORD,
+            font=self.customFont,
+
+        )
+
+        editArea.pack(padx=3, pady=3, fill=tk.BOTH, expand=True)
+        editArea.focus_set()
+
+        menubar = tk.Menu(win)
+        # create a pulldown menu, and add it to the menu bar
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Open",)
+        filemenu.add_command(label="Save",)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit")
+        menubar.add_cascade(label="File",  menu=filemenu)
+        menubar.add_command(label="IncreaseFont", command=self.IncreaseFont)
+        menubar.add_command(label="DecreaseFont", command=self.DecreaseFont)
+        win.config(menu=menubar)
 
 
-win = tk.Tk()
-customFont = tkFont.Font(family="Helvetica", size=14)
+        win.mainloop()
 
-frame1 = tk.Frame(
-    master = win,
-    #background = 'maroon'
-)
-frame1.pack(fill='both', expand='yes')
-editArea = tkst.ScrolledText(
-    master = frame1,
-    wrap   = tk.WORD,
-    font = customFont,
+    def IncreaseFont(self):
+        size = self.customFont['size']
+        self.customFont.configure(size=size + 2)
 
-)
-
-editArea.pack(padx=3, pady=3, fill=tk.BOTH, expand=True)
-editArea.focus_set()
-win.mainloop()
+    def DecreaseFont(self):
+        size = self.customFont['size']
+        if size-2>0:
+            self.customFont.configure(size=size - 2)
+app=App()
