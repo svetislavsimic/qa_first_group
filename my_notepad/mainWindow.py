0ef221ba4b2b668d175e4c6c789afbc3797613c9
sima <svetislavsimic77@gmail.com>
2019-02-20 20:32:44 +0100

    dodao Save i Open

--- my_notepad/mainWindow.py
+++ my_notepad/mainWindow.py
@@ -2,34 +2,36 @@ import tkinter as tk
 import tkinter.scrolledtext as tkst
 import tkinter.font as tkFont
 from tkinter import messagebox
+from tkinter import filedialog
+import os
 
 class App:
-    def __init__(self):
+    def __init__(self,filename=None):
         self.win = tk.Tk()
         self.customFont = tkFont.Font(
             family="Helvetica", size=14
         )
+        self.filename=filename
 
         frame1 = tk.Frame(
             master=self.win,
             # background = 'maroon'
         )
         frame1.pack(fill='both', expand='yes')
-        editArea = tkst.ScrolledText(
+        self.editArea = tkst.ScrolledText(
             master=frame1,
             wrap=tk.WORD,
             font=self.customFont,
-
         )
 
-        editArea.pack(padx=3, pady=3, fill=tk.BOTH, expand=True)
-        editArea.focus_set()
+        self.editArea.pack(padx=3, pady=3, fill=tk.BOTH, expand=True)
+        self.editArea.focus_set()
 
         menubar = tk.Menu(self.win)
         # create a pulldown menu, and add it to the menu bar
         filemenu = tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Open",)
-        filemenu.add_command(label="Save",)
+        filemenu.add_command(label="Open", command=self.Open)
+        filemenu.add_command(label="Save", command=self.Save)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.Exit)
         menubar.add_cascade(label="File",  menu=filemenu)
@@ -52,11 +54,28 @@ class App:
             self.customFont.configure(size=size - 2)
 
     def Exit(self):
-        result = messagebox.askokcancel("Python","Would you like to save the data?")
+        result = messagebox.askyesnocancel(title="Python",message="Would you like to save the data?")
         if result:
             self.win.destroy()
         else:
             pass
 
+    def Open(self):
+        cwd = os.getcwd()
+        self.filename=filedialog.askopenfilename(initialdir=cwd, title="Select file",
+                                   filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
+        print(self.filename)
+        if not self.filename:
+            return
+        with open(self.filename,mode='r') as f:
+            self.editArea.insert('1.0', f.read())
+
+    def Save(self):
+        f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+        if f is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+            return
+        with f:
+            text2save = str(self.editArea.get(1.0, "end"))  # starts from `1.0`, not `0.0`
+            f.write(text2save)
 
 app=App()
